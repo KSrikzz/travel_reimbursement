bbf0f263ca05ef362d29383a6b5c14f84108db60
Kaushiq Srikanth S S <ksrikanth0760@gmail.com>
2026-09-22 11:07:42 +0530

    feat: connect mongodb atlas

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const dotenv = require("dotenv");
+const connectDB = require("./config/database");
 
 dotenv.config();
 
@@ -6,6 +7,12 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+const startServer = async () => {
+    await connectDB();
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+};
+
+startServer();
